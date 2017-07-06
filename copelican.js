c9bb1efdd943b96827d924623571fb8ae7c0efdb
fousseni <foussenicoulibalypro@gmail.com>
2017-07-06 06:52:38 +0200

    first commit

---
+++ copelican.js
@@ -0,0 +1,16 @@
+
+$(function(){
+  var header = $("header"),
+      yOffset = 0,
+      triggerPoint = 150;
+  $(window).scroll(function(){
+    yOffset = $(window).scrollTop();
+
+    if(yOffset >= triggerPoint){
+      header.addClass("minimized");
+    }else{
+      header.removeClass("minimized");
+    }
+
+  });
+});
